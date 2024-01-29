b90a8e3a3a8292e517483e5dbc20d3efd01224be
msholehhudin <msholehhudin.ms@gmail.com>
2024-01-29 19:06:54 +0700

    First Commit

--- components/Nav.js
+++ components/Nav.js
@@ -6,28 +6,58 @@ import {
   HiRectangleGroup,
   HiChatBubbleBottomCenterText,
   HiEnvelope,
-} from 'react-icons/hi2';
+} from "react-icons/hi2";
 
 // nav data
 export const navData = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
   {
-    name: 'testimonials',
-    path: '/testimonials',
+    name: "testimonials",
+    path: "/testimonials",
     icon: <HiChatBubbleBottomCenterText />,
   },
   {
-    name: 'contact',
-    path: '/contact',
+    name: "contact",
+    path: "/contact",
     icon: <HiEnvelope />,
   },
 ];
 
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathName = router.pathname;
+  return (
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:h-screen">
+      {/* inner */}
+      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] bg-white/10 xl:h-max py-8 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
+        {navData.map((link, index) => (
+          <Link
+            className={`${
+              link.path === pathName && "text-accent"
+            } hover:text-accent transition-all duration-300 relative flex items-center group`}
+            href={link.path}
+            key={index}
+          >
+            <div className="absolute pr-14 right-0 xl:group-hover:flex hidden">
+              <div className="bg-white relative flex text-primary items-center p-[6px] rounded-[3px]">
+                <div className="text-[12px] font-semibold capitalize leading-none">
+                  {link.name}
+                </div>
+                <div className="border-solid border-l-white border-l-8 border-y-transparent  border-y-[6px]  absolute -right-1"></div>
+              </div>
+            </div>
+            <div>{link.icon}</div>
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,11 @@
+import Image from "next/image";
+
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+  return (
+    <div className="absolute mix-blend-color-dodge left-0 top-0 z-10 w-[200px] xl:w-[400px] opacity-50">
+      <Image src="/top-left-img.png" width={400} height={400} alt="" />
+    </div>
+  );
 };
 
 export default TopLeftImg;
